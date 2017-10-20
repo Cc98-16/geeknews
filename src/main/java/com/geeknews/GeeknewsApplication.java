a358b904460b76a70ba8e1a9c5c880e99d0a1e3e
陈深瑜 <821413249@qq.com>
2017-10-20 15:34:25 +0800

    Initial commit

---
+++ src/main/java/com/geeknews/GeeknewsApplication.java
@@ -0,0 +1,12 @@
+package com.geeknews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeknewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeknewsApplication.class, args);
+	}
+}
