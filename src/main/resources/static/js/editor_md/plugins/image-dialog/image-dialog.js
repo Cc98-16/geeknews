36b34b885d0a18cadefe8ca5d104102a8cb02f40
陈深瑜 <821413249@qq.com>
2017-10-25 09:00:02 +0800

    修改Markdown

--- src/main/resources/static/js/editor_md/plugins/image-dialog/image-dialog.js
+++ src/main/resources/static/js/editor_md/plugins/image-dialog/image-dialog.js
@@ -46,11 +46,8 @@
                     action += "&callback=" + settings.uploadCallbackURL + "&dialog_id=editormd-image-dialog-" + guid;
                 }
 
-				//new
-				var dialogContent = "<div id=\"wx_browser_panel\"></div>";
-                dialogContent += ( (settings.imageUpload) ? "<form action=\"" + action +"\" target=\"" + iframeName + "\" method=\"post\" enctype=\"multipart/form-data\" class=\"" + classPrefix + "form\">" : "<div class=\"" + classPrefix + "form\">" ) +
+                var dialogContent = ( (settings.imageUpload) ? "<form action=\"" + action +"\" target=\"" + iframeName + "\" method=\"post\" enctype=\"multipart/form-data\" class=\"" + classPrefix + "form\">" : "<div class=\"" + classPrefix + "form\">" ) +
                                         ( (settings.imageUpload) ? "<iframe name=\"" + iframeName + "\" id=\"" + iframeName + "\" guid=\"" + guid + "\"></iframe>" : "" ) +
-										"<div class='imgsite'>"+
                                         "<label>" + imageLang.url + "</label>" +
                                         "<input type=\"text\" data-url />" + (function(){
                                             return (settings.imageUpload) ? "<div class=\"" + classPrefix + "file-input\">" +
@@ -58,17 +55,15 @@
                                                                                 "<input type=\"submit\" value=\"" + imageLang.uploadButton + "\" />" +
                                                                             "</div>" : "";
                                         })() +
-                                        "</div>" +
-										"<div class='imgalt'>" +
+                                        "<br/>" +
                                         "<label>" + imageLang.alt + "</label>" +
                                         "<input type=\"text\" value=\"" + selection + "\" data-alt />" +
-                                        "</div>" +
-										"<div class='imglink'>" +
+                                        "<br/>" +
                                         "<label>" + imageLang.link + "</label>" +
                                         "<input type=\"text\" value=\"http://\" data-link />" +
-                                        "</div>" +
+                                        "<br/>" +
                                     ( (settings.imageUpload) ? "</form>" : "</div>");
-				
+
                 //var imageFooterHTML = "<button class=\"" + classPrefix + "btn " + classPrefix + "image-manager-btn\" style=\"float:left;\">" + imageLang.managerButton + "</button>";
 
                 dialog = this.createDialog({
@@ -125,76 +120,6 @@
                 });
 
                 dialog.attr("id", classPrefix + "image-dialog-" + guid);
-				
-				//new
-				dialog.css("height","");
-				dialog.css("width","680px");
-				/*$.post("files.html", {}, function(result){
-					$("#wx_browser_panel").html(result);
-					var newtop = ($(window).height() - dialog.height()) / 2;
-					if (newtop <= 0) {
-						newtop = 30;
-					}
-					var dialogPosition = function(){
-						dialog.css({
-							top    : newtop + "px",
-							left   : ($(window).width() - dialog.width()) / 2 + "px"
-						});
-					};
-					dialogPosition();
-				});*/
-				
-				//newnew
-				//照片框中的所有链接都重新加载此框
-				var page = 1;
-				function reloadlist() {//loadurl, page//, callback
-					var loadurl = arguments[0] || "";
-					//page = arguments[1] || page;
-					//var callback = arguments[2] || function(){};
-					var param = {};
-					if (typeof arguments[1] === "undefined") {//typeof(arguments[1]) != "undefined"
-					}else{
-						page = arguments[1] || page;
-						param = {page:page};
-					}
-					$("#wx_browser_panel").load(loadurl, param, function(){
-						var newtop = ($(window).height() - dialog.height()) / 2;
-						if (newtop <= 0) {
-							newtop = 30;
-						}
-						var dialogPosition = function(){
-							dialog.css({
-								top    : newtop + "px",
-								left   : ($(window).width() - dialog.width()) / 2 + "px"
-							});
-						};
-						dialogPosition();
-					});
-				}
-				
-				$("#wx_browser_panel").on("click","a",function(e){
-					e.preventDefault();
-					e.stopPropagation();
-					//console.log("dddd");
-					try {
-						if (typeof($(this).data("page")) != "undefined" && $(this).data("page") > 0) {//翻页的链接上有page属性
-							page = $(this).data("page");
-							reloadlist($(this).attr("href"), page);
-						} else {
-							reloadlist($(this).attr("href"));
-						}
-					}catch(e){};
-				});
-				
-				reloadlist("loadalbums.htm?type=editor", page);
-				//newnew
-				
-				$(document).on("click","[data-bwfile]",function(){
-					//console.log($(this));
-					dialog.find("[data-url]").val($(this).data("bwfile"));
-					dialog.find("[data-alt]").val($(this).data("bwtitle"));
-				});
-				//new
 
 				if (!settings.imageUpload) {
                     return ;
@@ -209,14 +134,14 @@
 					if (fileName === "")
 					{
 						alert(imageLang.uploadFileEmpty);
-                        
+
                         return false;
 					}
-					
+
                     if (!isImage.test(fileName))
 					{
 						alert(imageLang.formatNotAllowed + settings.imageFormats.join(", "));
-                        
+
                         return false;
 					}
 
@@ -227,7 +152,7 @@
                         var uploadIframe = document.getElementById(iframeName);
 
                         uploadIframe.onload = function() {
-                            
+
                             loading(false);
 
                             var body = (uploadIframe.contentWindow ? uploadIframe.contentWindow : uploadIframe.contentDocument).document.body;
@@ -235,13 +160,16 @@
 
                             json = (typeof JSON.parse !== "undefined") ? JSON.parse(json) : eval("(" + json + ")");
 
-                            if (json.success === 1)
+                            if(!settings.crossDomainUpload)
                             {
-                                dialog.find("[data-url]").val(json.url);
-                            }
-                            else
-                            {
-                                alert(json.message);
+                              if (json.success === 1)
+                              {
+                                  dialog.find("[data-url]").val(json.url);
+                              }
+                              else
+                              {
+                                  alert(json.message);
+                              }
                             }
 
                             return false;
