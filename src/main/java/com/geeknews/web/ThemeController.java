52270e476e37b82738d481eb4b7110c0455864fa
陈深瑜 <821413249@qq.com>
2017-11-06 14:37:01 +0800

    新增全部主题显示界面

--- src/main/java/com/geeknews/web/ThemeController.java
+++ src/main/java/com/geeknews/web/ThemeController.java
@@ -6,11 +6,14 @@ import javax.annotation.Resource;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.geeknews.domain.Theme;
 import com.geeknews.service.ThemeService;
+import com.geeknews.utils.MyPage;
 import com.geeknews.valid.ThemeForm;
 
 @Controller
@@ -30,5 +33,12 @@ public class ThemeController extends BaseController{
 		String tname = themeForm.getThemename();
 		themeService.ThemeSave(themeForm,tname);
 		return null;
+	}
+	
+	@GetMapping(value = "/theme")
+	public String ThemeIndex(Model model){
+		MyPage<Theme> theme = themeService.findAll(keyword, page, pagesize);
+		model.addAttribute("ps",theme);
+		return "theme";
 	}
 }
